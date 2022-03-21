7d2475e4f254498ed4fb607762879b67dda30663
Naoaki Iwakiri <naokiri@gmail.com>
2022-03-21 21:04:05 +0900

    block public access to terraform backend

--- import.sh
+++ import.sh
@@ -5,6 +5,7 @@ OIDC_ARN="arn:aws:iam::THENUMBER:oidc-provider/token.actions.githubusercontent.c
 
 terraform import aws_s3_bucket.infra $BUCKET_NAME
 terraform import aws_s3_bucket_versioning.infra_versioning $BUCKET_NAME
+terraform import aws_s3_bucket_public_access_block.infra_public_access $BUCKET_NAME
 terraform import aws_s3_bucket_server_side_encryption_configuration.infra_encryption $BUCKET_NAME
 terraform import aws_iam_openid_connect_provider.github_oidc $OIDC_ARN
 terraform import aws_iam_role.github_role GithubRole
--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
       version = "~> 4.5.0"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = "~> 3.1.0"
     }
   }
@@ -15,12 +15,12 @@ terraform {
 
 variable "private" {
   description = "Just hidden not to expose on public repo"
-  type =object({
-    assume_role_arn = string
-    infra_bucket_name = string
+  type        = object({
+    assume_role_arn                = string
+    infra_bucket_name              = string
     # Strings like "repo:naokiri/reponame:*"
     # See https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/about-security-hardening-with-openid-connect#example-subject-claims
-    allowed_subs = list(string)
+    allowed_subs                   = list(string)
     terraform_operation_policy_arn = list(string)
   })
 }
@@ -38,30 +38,30 @@ data "tls_certificate" "oidc_certificate" {
 }
 
 resource "aws_iam_openid_connect_provider" "github_oidc" {
-  url = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list =[data.tls_certificate.oidc_certificate.certificates[0].sha1_fingerprint]
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.oidc_certificate.certificates[0].sha1_fingerprint]
 }
 
 resource "aws_iam_role" "github_role" {
-  name = "GithubRole"
-  description = "Role to assume from github"
+  name                = "GithubRole"
+  description         = "Role to assume from github"
   managed_policy_arns = var.private.terraform_operation_policy_arn
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+  assume_role_policy  = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Principal": {
-          "Federated": aws_iam_openid_connect_provider.github_oidc.arn
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : aws_iam_openid_connect_provider.github_oidc.arn
         },
-        "Action": "sts:AssumeRoleWithWebIdentity",
-        "Condition": {
-          "StringEquals": {
-            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
           }
-          "StringLike": {
-            "token.actions.githubusercontent.com:sub": var.private.allowed_subs
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : var.private.allowed_subs
           }
         }
       }
@@ -84,13 +84,22 @@ resource "aws_s3_bucket_versioning" "infra_versioning" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "infra_public_access" {
+  bucket = aws_s3_bucket.infra.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "infra_encryption" {
   bucket = aws_s3_bucket.infra.id
   rule {
     bucket_key_enabled = true
 
     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
+      sse_algorithm = "AES256"
     }
   }
 }
